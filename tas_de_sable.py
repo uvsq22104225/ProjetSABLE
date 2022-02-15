79554cc923f83e52a8668fb843dc01ccbfc18e8b
Hugo CORAL <hugo.coral@ens.uvsq.fr>
2022-02-15 11:54:11 +0100

    Push de la mort

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -31,20 +31,13 @@ WIDTH = 500
 #       DEFINITION DES FONCTIONS        #
 #########################################
 
-def canevas():
-    """ Initilise le terrain de la manière suivante:
-    * met à 0 la liste appelée terrain à 2D qui contient pour chaque case la 
-    valeur 1 si il y a un mur, et 0 sinon
-    * initialise la liste grille à 2D qui contient l'identifiant
-    du carré dessiné sur le canevas pour chaque case 
-    """
-    racine = tk.Tk() # Création de la fenêtre racine
-    bouton = tk.Button(text="ouverture de la fenetre", 
+racine = tk.Tk() # Création de la fenêtre racine
+bouton = tk.Button(text="Ouverture de la fenetre", 
                         font = ("Helvetica", "20")
                         )
-    bouton.grid(column=0, row=1)
-    canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
-    canvas.grid(column=0, row=0)
-    # on récupère l'identifiant du cercle:
-    cercle = canvas.create_oval((100, 100), (300, 300), fill="blue", width=5, outline="cyan") 
-    racine.mainloop() # Lancement de la boucle principale
+bouton.grid(column=0, row=1)
+canvas = tk.Canvas(racine, bg="Black", height=HEIGHT, width=WIDTH)
+canvas.grid(column=0, row=0)
+# on récupère l'identifiant du cercle:
+Ligne = canvas.create_line(166,0,166,500, fill="white", width=5) 
+racine.mainloop() # Lancement de la boucle principale
