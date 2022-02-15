579f09adb5088ad0a66d8db47a25586a31bf5c6f
SOPHIE HATEY <sophie.hatey@gmail.com>
2022-02-15 11:42:18 +0100

    copie2

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -11,40 +11,40 @@
 #########################################
 
 
-import tkinter as Tk
+import tkinter as tk
 
 
 #########################################
 #       DEFINITION DES CONSTANTES       #
 #########################################
 
-
+HEIGHT = 500
+WIDTH = 500
 
 #########################################
 #  DEFINITION DES VARIABLES GLOBALES    #
 #########################################
 
 
+
 #########################################
 #       DEFINITION DES FONCTIONS        #
 #########################################
 
-
-import tkinter as tk
-
-HEIGHT = 500
-WIDTH = 500
-
-def bouge_cercle():
-    canvas.move(cercle, 50, 50) # méthode qui déplace l'objet cercle
-
-racine = tk.Tk() # Création de la fenêtre racine
-bouton = tk.Button(text="déplace cercle", 
-                    command=bouge_cercle, font = ("Helvetica", "30")
-                  )
-bouton.grid(column=0, row=1)
-canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
-canvas.grid(column=0, row=0)
-# on récupère l'identifiant du cercle:
-cercle = canvas.create_oval((100, 100), (300, 300), fill="blue", width=5, outline="cyan") 
-racine.mainloop() # Lancement de la boucle principale
+def canevas():
+    """ Initilise le terrain de la manière suivante:
+    * met à 0 la liste appelée terrain à 2D qui contient pour chaque case la 
+    valeur 1 si il y a un mur, et 0 sinon
+    * initialise la liste grille à 2D qui contient l'identifiant
+    du carré dessiné sur le canevas pour chaque case 
+    """
+    racine = tk.Tk() # Création de la fenêtre racine
+    bouton = tk.Button(text="ouverture de la fenetre", 
+                        font = ("Helvetica", "20")
+                        )
+    bouton.grid(column=0, row=1)
+    canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
+    canvas.grid(column=0, row=0)
+    # on récupère l'identifiant du cercle:
+    cercle = canvas.create_oval((100, 100), (300, 300), fill="blue", width=5, outline="cyan") 
+    racine.mainloop() # Lancement de la boucle principale
