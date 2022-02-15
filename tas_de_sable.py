a2d737faf634259c87f8a70335d9fdcc5970e34b
Hugo CORAL <hugo.coral@ens.uvsq.fr>
2022-02-15 10:28:56 +0100

    Mise en place du premier fichier

---
+++ tas_de_sable.py
@@ -0,0 +1,7 @@
+#########################################
+# groupe BI TD03 GROUPE 1
+# Sophie Hatey
+# Hugo Coral
+# Jibril Bedoei
+# https://github.com/uvsq22104225/ProjetSABLE
+#########################################
