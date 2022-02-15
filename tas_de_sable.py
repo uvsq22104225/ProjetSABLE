bdc2ed38c6cc16ec3c19b7543880fdd3930acbbf
SOPHIE HATEY <sophie.hatey@gmail.com>
2022-02-15 11:32:17 +0100

    copie

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -28,3 +28,23 @@ import tkinter as Tk
 #########################################
 #       DEFINITION DES FONCTIONS        #
 #########################################
+
+
+import tkinter as tk
+
+HEIGHT = 500
+WIDTH = 500
+
+def bouge_cercle():
+    canvas.move(cercle, 50, 50) # méthode qui déplace l'objet cercle
+
+racine = tk.Tk() # Création de la fenêtre racine
+bouton = tk.Button(text="déplace cercle", 
+                    command=bouge_cercle, font = ("Helvetica", "30")
+                  )
+bouton.grid(column=0, row=1)
+canvas = tk.Canvas(racine, bg="red", height=HEIGHT, width=WIDTH)
+canvas.grid(column=0, row=0)
+# on récupère l'identifiant du cercle:
+cercle = canvas.create_oval((100, 100), (300, 300), fill="blue", width=5, outline="cyan") 
+racine.mainloop() # Lancement de la boucle principale
